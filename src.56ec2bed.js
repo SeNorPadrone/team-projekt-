2a4aecdb5c037187cf876c6b28eead7706b10171
SeNorPadrone <90000708+SeNorPadrone@users.noreply.github.com>
2021-12-03 19:15:15 +0000

    Deploying to gh-pages from @ SeNorPadrone/team-projekt-@be9377d92d255f7b58f8f5a572169e4ccd77c478 🚀

--- src.56ec2bed.js
+++ src.56ec2bed.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-projekt-/src.b2eba873.js.map
+//# sourceMappingURL=/team-projekt-/src.56ec2bed.js.map
