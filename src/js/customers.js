d2254f91ff3589eeceddcf71c266ce4737d476ff
SeNorPadrone <90000708+SeNorPadrone@users.noreply.github.com>
2021-12-06 22:21:48 +0200

    15
    15

--- src/js/customers.js
+++ src/js/customers.js
@@ -1,4 +1,5 @@
-var slideIndex = 1;
+
+    var slideIndex = 1;
     showSlides(slideIndex);
 
     function plusSlides(n) {
@@ -27,19 +28,5 @@ var slideIndex = 1;
       }
       slides[slideIndex - 1].style.display = 'block';
       dots[slideIndex - 1].className += ' active';
-    }
-
- var slideIndex = 0;
- showSlides();
-
- function showSlides() {
-     var i;
-     var slides = document.getElementsByClassName("reviews__slideshow");
-     for (i = 0; i < slides.length; i++) {
-         slides[i].style.display = "none";
-     }
-     slideIndex++;
-     if (slideIndex > slides.length) {slideIndex = 1}
-     slides[slideIndex-1].style.display = "block";
-     setTimeout(showSlides, 2000); // Change image every 2 seconds
- } 
+}
+    
