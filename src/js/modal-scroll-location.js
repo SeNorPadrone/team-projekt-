87197866744d3f53b2039c9d92071542768ef952
Oleksandr Soloviov <shakal194@gmail.com>
2021-12-07 22:59:20 +0200

    fixed

--- src/js/modal-scroll-location.js
+++ src/js/modal-scroll-location.js
@@ -1,7 +1,7 @@
 (() => {
-  const mobileMenu = document.querySelector('[data-modal]');
-  const openMenuBtn = document.querySelector('[data-modal-open]');
-  const closeMenuBtn = document.querySelector('[data-modal-close]');
+  const mobileMenu = document.querySelector('[data-modal-location]');
+  const openMenuBtn = document.querySelector('[data-modal-location-open]');
+  const closeMenuBtn = document.querySelector('[data-modal-location-close]');
 
   const toggleMenu = () => {
     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
