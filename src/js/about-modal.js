647a59b7dee7b4e5669361c01cf7105d5a3e4e40
OliaChuzhdiuk <90050382+OliaChuzhdiuk@users.noreply.github.com>
2021-12-09 10:31:13 +0200

    1

--- src/js/about-modal.js
+++ src/js/about-modal.js
@@ -1,16 +1,15 @@
 (() => {
     const refs = {
-        openModalBtn: document.querySelector('[data-about-modal-open]'),
-        closeModalBtn: document.querySelector('[data-about-modal-close]'),
-        modal: document.querySelector('[data-about-modal]'),
-        bodyScrollLock: document.querySelector('[data-modal-scroll-lock]')
+        openModalBtn: document.querySelector('[about-modal-open]'),
+        closeModalBtn: document.querySelector('[about-modal-close]'),
+        modal: document.querySelector('[about-modal]'),
     };
 
     refs.openModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
 
     function toggleModal() {
+        document.body.classList.toggle("modal-open");
         refs.modal.classList.toggle('is-hidden');
-        refs.bodyScrollLock.classList.toggle('modal-open');
     }
 })();
